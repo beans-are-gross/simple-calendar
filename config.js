7ce1728d75e1d9b13098f912d299a0ee12ee3f50
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2023-10-11 08:04:11 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// calendar config
+
+// clock image (only accepts local or external urls)
+export const clockImage = "https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1575&q=80";
+
+// clock font color (accepts simple color names and hex codes)
+export const clockFontColor = "white";
+
+// calendar background color (accepts simple color names and hex codes)
+export const calBackgroundColor = "lightgrey";
+
+// calendar font color (accepts simple color names and hex codes)
+export const calFontColor = "black";
+
+// calendar current date color (accepts simple color names and hex codes)
+export const dateColor = "green";
---
+++ script.js
@@ -0,0 +1,68 @@
+import { clockImage, clockFontColor, calBackgroundColor, calFontColor, dateColor } from "./config.js";
+
+const time = document.getElementById('cal-time');
+const date = document.getElementById('cal-date');
+
+function updateClock() {
+  // clock
+  document.getElementById("cal-clock-container").style.backgroundImage = "URL(" + clockImage + ")";
+  document.getElementById("cal-clock-container").style.color = clockFontColor;
+
+  document.getElementById("cal-table").style.backgroundColor = calBackgroundColor;
+  document.getElementById("cal-table").style.color = calFontColor;
+
+  
+  var d = new Date();
+
+  var year = d.getFullYear();
+  var monthsList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var month = monthsList[d.getMonth()];
+  var day = d.getDate();
+  var daysList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var dayOfWeek = daysList[d.getDay()];
+
+  var hours = d.getHours();
+  var mins = d.getMinutes();
+  var seconds = d.getSeconds();
+  var timeOfDay = "AM";
+
+  if (hours > 11){
+    if (hours !== 12){
+      hours = hours - 12;
+    }
+    timeOfDay = "PM";
+  }
+
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  time.textContent = hours + ":" + mins + ":" + seconds + " " + timeOfDay;
+
+  date.textContent = dayOfWeek + ", " + month + " " + day + ", " + year;
+
+  // calendar
+
+  var f = new Date(year, d.getMonth(), 1);
+  var fDayOfWeek = f.getDay() + 1;
+
+  var totalDays = new Date(year, d.getMonth(), 0).getDate();
+
+  var calIndex = fDayOfWeek;
+  var calDay = 1;
+
+  while (calDay < totalDays + 1){
+    if(calDay == day){
+      document.getElementById(calIndex).style.backgroundColor = dateColor;
+    }
+    document.getElementById(calIndex).textContent = calDay;
+    calIndex ++;
+    calDay ++;
+  }
+}
+
+setInterval(updateClock, 1000);
